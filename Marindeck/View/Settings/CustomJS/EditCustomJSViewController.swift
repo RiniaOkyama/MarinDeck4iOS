4b1f38ed587cf75da123d7e6740f67eca362a160
riniaokyama <rinia.sns@gmail.com>
2021-04-16 20:35:56 +0900

    fix: customjs keyboard close bug fix

--- Marindeck/View/Settings/CustomJS/EditCustomJSViewController.swift
+++ Marindeck/View/Settings/CustomJS/EditCustomJSViewController.swift
@@ -88,4 +88,7 @@ class EditCustomJSViewController: UIViewController {
         updateJS()
     }
     
+    @IBAction func keyboardDonePressed(_ sender: Any) {
+        textView.resignFirstResponder()
+    }
 }
