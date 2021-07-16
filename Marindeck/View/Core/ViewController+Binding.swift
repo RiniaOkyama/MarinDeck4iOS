ea1ed4eefa95c190bcca2bd93f0c60c674a6dea9
riniaokyama <rinia.sns@gmail.com>
2021-07-16 15:55:59 +0900

    リファクタリグ

---
+++ Marindeck/View/Core/ViewController+Binding.swift
@@ -0,0 +1,36 @@
+//
+// Created by craptone on 2021/07/16.
+//
+
+
+extension ViewController {
+    func focusTweetTextArea() {
+        webView.evaluateJavaScript("document.querySelector(\"body > div.application.js-app.is-condensed.hide-detail-view-inline > div.js-app-content.app-content.is-open > div:nth-child(1) > div > div > div > div > div > div.position-rel.compose-text-container.padding-a--10.br--4 > textarea\").focus()") { object, error in
+            print("focusTweetTextArea : ", error ?? "成功")
+        }
+    }
+
+    func positionTweetLike(x: Int, y: Int) {
+        webView.evaluateJavaScript("touchPointTweetLike(\(x), \(y))", completionHandler: { object, error in
+            print("touchPointTweetLike : ", error ?? "成功")
+        })
+    }
+
+    func isColumnScroll(_ bool: Bool) {
+        let isScroll = bool ? "on" : "off"
+        webView.evaluateJavaScript("columnScroll.\(isScroll)()") { object, error in
+            print("webViewLog : ", error ?? "成功")
+        }
+    }
+
+    func getUserNameID() -> (String, String) {
+        let userName = webView.evaluate(javaScript: "document.querySelector('body > div.application.js-app > header > div > div.js-account-summary > a > div > div > div > span').innerText") as? String ?? "unknown"
+        let userID = webView.evaluate(javaScript: "document.querySelector('body > div.application.js-app > header > div > div.js-account-summary > a > div > div > span').innerText") as? String ?? "@unknown"
+        return (userName, userID)
+    }
+
+    func getUserIcon() -> String {
+        return (webView.evaluate(javaScript: "document.querySelector('body > div.application.js-app.is-condensed > header > div > div.js-account-summary > a > div > img').src") as? String) ?? "https://pbs.twimg.com/media/Ewk-ESrUYAAZYKe?format=jpg&name=medium"
+    }
+
+}
--- Marindeck/View/Core/ViewCotroller+ViewSetup.swift
+++ Marindeck/View/Core/ViewCotroller+ViewSetup.swift
@@ -9,7 +9,7 @@ import UIKit
 import WebKit
 
 extension ViewController {
-    
+
     func setupView() {
         self.view.backgroundColor = #colorLiteral(red: 0.08181380481, green: 0.1257319152, blue: 0.1685300171, alpha: 1)
         self.bottomBackView.backgroundColor = #colorLiteral(red: 0.08181380481, green: 0.1257319152, blue: 0.1685300171, alpha: 1)
@@ -19,7 +19,7 @@ extension ViewController {
         imageView.clipsToBounds = true
         imageView.layer.cornerRadius = 15
         imageView.contentMode = .scaleAspectFill
-        
+
 //        tweetFloatingBtn.layer.cornerRadius = tweetFloatingBtn.frame.width / 2
         tweetFloatingBtn.layer.shadowOffset = CGSize(width: 0.0, height: 2.0)
         tweetFloatingBtn.layer.shadowColor = UIColor.black.cgColor
@@ -28,7 +28,7 @@ extension ViewController {
 //        tweetFloatingBtn.imageEdgeInsets = UIEdgeInsets(top: 30, left: 30, bottom: 30, right: 30)
         tweetFloatingBtn.setImage(UIImage(named: "tweet")!)
         tweetFloatingBtn.tapped = tweetPressed
-        
+
         let debugAction = NDTweetBtnAction(
                 image: UIImage(systemName: "ladybug")!,
                 handler: { (NDTweetBtnAction) -> Void in
@@ -48,36 +48,36 @@ extension ViewController {
         tweetFloatingBtn.addAction(action: debugAction)
         tweetFloatingBtn.addAction(action: gifAction)
         tweetFloatingBtn.addAction(action: tweetAction)
-        
+
         tweetFloatingBtn.isHidden = true
-        
-        
+
+
         mainDeckView.backgroundColor = .none
-        
+
         mainDeckBlurView = UIView(frame: CGRect(origin: .zero, size: view.bounds.size))
         mainDeckBlurView.backgroundColor = .none
         mainDeckBlurView.isUserInteractionEnabled = false
-        
+
 //        menuView.translatesAutoresizingMaskIntoConstraints = true
 //        mainDeckView.translatesAutoresizingMaskIntoConstraints = true
-        
+
         NotificationCenter.default.addObserver(self, selector: #selector(onOrientationDidChange(notification:)), name: UIDevice.orientationDidChangeNotification, object: nil)
-        
+
         // FIXME
         menuVC = self.children[0] as! MenuViewController
         menuVC.delegate = self
-        
+
         self.view.addSubview(loadingIndicator)
         loadingIndicator.startAnimating()
-        
+
 
         setupWebView()
-        
+
         self.view.addSubview(mainDeckBlurView)
-        
+
         setupWebViewToolBar()
     }
-    
+
     func setupWebView() {
         let webConfiguration = WKWebViewConfiguration()
 
@@ -95,12 +95,12 @@ extension ViewController {
 
 //        mainDeckView.backgroundColor = .red
         webView = WKWebView(frame: mainDeckView.bounds, configuration: webConfiguration)
-        
+
         // あほくさ
         webView.frame.size.width = view.frame.width
         webView.frame.size.height = view.frame.height - (topBackView.frame.height + bottomBackView.frame.height)
         webView.frame.origin.y = topBackView.frame.height
-        
+
         webView.uiDelegate = self
         webView.navigationDelegate = self
         webView.scrollView.showsVerticalScrollIndicator = false
@@ -116,7 +116,7 @@ extension ViewController {
         let edgePan = UIScreenEdgePanGestureRecognizer(target: self, action: #selector(panTop))
         edgePan.edges = .left
         edgePan.delegate = self
-        
+
         self.webView.backgroundColor = #colorLiteral(red: 0.08181380481, green: 0.1257319152, blue: 0.1685300171, alpha: 1)
         webView.addGestureRecognizer(edgePan)
         let interaction = UIContextMenuInteraction(delegate: self)
@@ -130,16 +130,16 @@ extension ViewController {
         webView.load(myRequest)
         self.mainDeckView.addSubview(webView)
     }
-    
+
     func setupWebViewToolBar() {
         let toolbar = UIToolbar(frame: CGRect(x: 0, y: 0, width: 320, height: 44))
-        let doneItem = UIBarButtonItem(title: "完了", style: .done, target: self, action: #selector(dismissKeyboard) )
+        let doneItem = UIBarButtonItem(title: "完了", style: .done, target: self, action: #selector(dismissKeyboard))
         let flexibleSpaceItem = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: self, action: nil)
         let iconSpaceItem = UIBarButtonItem(barButtonSystemItem: .fixedSpace, target: nil, action: nil)
         iconSpaceItem.width = 8
         let gifItem = UIBarButtonItem(image: UIImage(named: "gif"), style: .plain, target: self, action: #selector(openSelectGif))
         let photoItem = UIBarButtonItem(image: UIImage(named: "photo"), style: .plain, target: self, action: #selector(openSelectPhoto))
-        
+
         gifItem.tintColor = .labelColor
         photoItem.tintColor = .labelColor
         doneItem.tintColor = .labelColor
@@ -150,5 +150,5 @@ extension ViewController {
 
         webView.addIndexAccessoryView(toolbar: toolbar)
     }
-    
+
 }
