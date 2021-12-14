07aa4a28a0075c35c4cda6ed7fa4a769ae6dc511
riniaokyama <rinia.sns@gmail.com>
2021-12-14 19:34:07 +0900

    [update](#17): update

--- Marindeck/Model/TweetDeck.swift
+++ Marindeck/Model/TweetDeck.swift
@@ -11,33 +11,61 @@
 //import Foundation
 //import WebKit
 //
-//final class TD {
+//
+//class TDBase {
+//    weak var webView: WKWebView?
+//}
+//
+//final class TD: TDBase {
 //    static let shared = TD()
-//    
-//    public var webView: WKWebView?
-//    
+//
+////    public weak var webView: WKWebView?
+//
 //    private(set) var account: Account? = nil
-//    
-//    private(set) let action = 
-//    
-//    
+//
+//    //    private(set) let action: A
+//    private(set) var settings: Settings? = nil
+//
+//
+//
+//
 //    func update(completion: (() -> ())? = nil) {
 //        webView?.evaluateJavaScript("TD.storage.accountController.getDefault().state") { [weak self] (obj, error) in
 //            guard let dict = obj as? [String: String] else { return }
 //            self?.account = Account(name: dict["name"], profileImageUrl: dict["profileImageURL"], userId: dict["username"], username: dict["username"])
 //        }
 //    }
-//    
-//    
+//
+//
 //    struct Account {
 //        var name: String?
 //        var profileImageUrl: String?
 //        var userId: String?
 //        var username: String?
 //    }
-//    
+//
 //    class Action {
-//        
+//        fileprivate weak var webView: WKWebView?
+//
 //    }
+//
+//    class Settings: TDBase {}
+//
 //}
 //
+//extension TD.Settings {
+//
+//    var theme: Theme? {
+//        set {
+//            theme = newValue
+//        }
+//        get {
+//            .dark
+//        }
+//    }
+//
+//    enum Theme: String {
+//        case light = "light"
+//        case dark = "dark"
+//    }
+//}
