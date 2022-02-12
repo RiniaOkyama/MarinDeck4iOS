9433174ee1dac04ed57d464729418c75ee1371e4
riniaokyama <rinia.sns@gmail.com>
2022-02-12 23:11:23 +0900

    add DangerTools

---
+++ DangerTools/Package.swift
@@ -0,0 +1,10 @@
+// swift-tools-version:5.1
+import PackageDescription
+
+let package = Package(
+    name: "DangerTools",
+    dependencies: [
+        .package(url: "https://github.com/realm/SwiftLint.git", from: "0.45.1"),
+    ],
+    targets: [.target(name: "DangerTools", path: "")]
+)
